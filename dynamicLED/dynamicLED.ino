e1079d7fb9a7cb99070cb4683a3d1974d5c179f5
Unknown <jrj8819@koreatech.ac.kr>
2018-12-02 19:01:39 +0900

    LED 아날로그 출력 예제 추가

---
+++ dynamicLED/dynamicLED.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 256; i++) {
+    analogWrite(9, i);
+    delay(10);
+  }
+
+  for(int i = 255; i > -1; i--) {
+    analogWrite(9, i);
+    delay(10);
+  }
+}
