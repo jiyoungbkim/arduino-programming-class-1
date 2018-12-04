9486066f3536440a291433373c95913846de709c
Unknown <jrj8819@koreatech.ac.kr>
2018-12-05 00:13:59 +0900

    가변저항을 활용하여 출력 시간 조절

--- example/doraemi/doraemi.ino
+++ example/doraemi/doraemi.ino
@@ -1,13 +1,14 @@
 int melody[] = {262, 294, 330, 349, 392, 440, 494, 523};
 
 void setup() {
-  for(int i = 0; i < sizeof(melody); i++) {
-    tone(8, melody[i], 250);
-    delay(400);
-    noTone(8);
-  }
+  
+  
+  
 }
 
 void loop() {
-
+  for(int i = 0; i < sizeof(melody); i++) {
+    tone(6, melody[i], 500);
+    delay(250);
+  }
 }
