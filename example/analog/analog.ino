67566d9d2d580231d6b3e4a6ce53911bd2413244
raejin <jrj8819@koreatech.ac.kr>
2018-12-04 15:44:30 +0900

    예제 수정 적용

--- example/analog/analog.ino
+++ example/analog/analog.ino
@@ -1,27 +1,14 @@
-int a;
-
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(9, 0);
-  delay(500);
-  analogWrite(9, 63);
-  delay(500);
-  analogWrite(9, 127);
-  delay(500);
-  analogWrite(9, 255);
-  delay(500);
-  analogWrite(9, 127);
-  delay(500);
-  analogWrite(9, 63);
-  delay(500);
-  analogWrite(9, 0);
-  delay(100);
 
-  a = analogRead(A0);
-  Serial.println(a);
+  for(int i = 0; i <= 255; i= i+10){
+    analogWrite(9, i);
+    delay(100);
+  }
+
+  for(int i = 255; i > 0; i= i-10) {
+    analogWrite(9, i);
+    delay(100);
+  }
 }
