db7aecd352646a11365d66a9663f29ce60a18ef7
Unknown <jrj8819@koreatech.ac.kr>
2018-12-04 23:11:43 +0900

    AnalogWrite함수로 피에조 출력하기

---
+++ example/piezoAnalogWrite/piezoAnalogWrite.ino
@@ -0,0 +1,16 @@
+int pzo = 6;
+int dly = 500;
+
+void setup() {
+  pinMode(pzo, OUTPUT);
+}
+
+void loop() {
+  analogWrite(pzo, 64);
+  delay(dly);
+  analogWrite(pzo, 128);
+  delay(dly);
+  analogWrite(pzo, 255);
+  delay(dly);
+  
+}
